ff90546f7933430e4c971c53e48790ec303d7e16
leslie <leslieaudreysipeuwou@gmail.com>
2022-06-17 13:48:23 +0100

    first commit

---
+++ TP1/exo1/exo1.c
@@ -0,0 +1,37 @@
+
+#include<stdio.h>
+
+int main(int argc, char* argv[])
+{
+
+
+	int nb= 0, i = -1;
+
+	FILE* f = fopen(argv[1],"r");
+	char T;
+	while((T=(char)getc(f))!=EOF)
+	{
+		if(T=='m'|| T=='u' || T=='r'){
+			if((T=='m')&&(i==-1)) {
+				i = 0;
+			}
+			if((T=='u')&&(i==0)) {
+				i = 1;
+			}
+			if((T=='r')&&(i==1)) {
+				i = 2;
+			}
+			if(i==2){
+				nb = nb+ 1;
+				i = -1;
+			} 
+		}
+		else {
+			i=-1;
+		}
+	}
+	fclose(f);
+	printf("Le nombre d'occurence est: %d\n",nb);
+
+	return 0;
+}
